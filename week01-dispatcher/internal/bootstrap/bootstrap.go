cdb58144fc10399dd66ed683f4ae7f3b133f4642
NARUBROWN <codingshark@inu.ac.kr>
2026-01-14 15:38:03 +0900

    feat: 1주차 과제

---
+++ week01-dispatcher/internal/bootstrap/bootstrap.go
@@ -0,0 +1,21 @@
+package bootstrap
+
+import (
+	"modern-web-framework-labs/week0-dispatcher/internal/dispatcher"
+	"modern-web-framework-labs/week0-dispatcher/internal/http"
+)
+
+/*
+bootstrap은 조립만 담당합니다.
+요청 처리 로직은 여기에 포함하지 않습니다.
+*/
+func Run() {
+	// HTTP Engine 생성 (한번 추상화된 EchoEngine 구조체 반환됨)
+	engine := http.NewEchoEngine()
+	// Dispatcher 생성
+	dispatcher := dispatcher.NewDispatcher()
+	// Engin에 Dispatcher 등록
+	engine.RegisterDispatcher(dispatcher)
+	// 서버 시작
+	engine.Start(":8080")
+}
---
+++ week01-dispatcher/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "modern-web-framework-labs/week0-dispatcher/internal/bootstrap"
+
+func main() {
+	bootstrap.Run()
+}
